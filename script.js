84d6a77dff7f08c1c90dfd853dee401d75203f7f
Abhideep Gupta <abhideep.gupta.g@gmail.com>
2025-06-08 18:45:58 +0530

    Add files via upload
    TranslateApp -  is a simple web-based language translator built with HTML, CSS, and JavaScript. It uses the MyMemory Translation API to translate text between multiple languages. The user-friendly interface lets you input text, choose source and target languages, and get translations instantly.

---
+++ script.js
@@ -0,0 +1,40 @@
+const selectTag = document.querySelectorAll("select");
+const translateBtn = document.querySelector("#transfer");
+const fromText = document.querySelector("#fromText");
+const toText = document.querySelector("#toText");
+
+
+
+
+selectTag.forEach((tag , id)=>{
+    
+
+    for (const countriesCode in countries) {
+        let selected ;
+        if( id == 0 && countriesCode == "en-GB"){
+            selected =" selected";
+        }else if(id == 1 && countriesCode == "hi-IN"){
+            selected =" selected";
+        }
+        let option = ` <option value="${countriesCode}" ${selected}>${countries[countriesCode]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+        }
+});
+
+translateBtn.addEventListener("click" , ()=> {
+  let Text = fromText.value,
+  translateFrom = selectTag[0].value,
+  translateTo = selectTag[1].value;
+
+  let apiURL= `https://api.mymemory.translated.net/get?q=${Text}!&langpair=${ translateFrom}|${translateTo}`;
+  
+  fetch(apiURL).then(res => res.json()).then(data =>{
+
+ 
+    toText.value = data.responseData.translatedText;
+});
+ 
+
+});
+
+
